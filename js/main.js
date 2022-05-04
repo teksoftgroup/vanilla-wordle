67384024bccee15d2e96e73011b1edb4f5f4bd0b
pascal <pascal.nglade@gmail.com>
2022-05-04 01:16:36 -0400

    add random selection of to the word from the wordbank

--- js/main.js
+++ js/main.js
@@ -6,16 +6,31 @@ document.addEventListener('DOMContentLoaded', () => {
     let guessedWords = [[]];
     let availableSpace = 1;
 
-    let word = 'dairy';
+    let word;
     let guessWordCount = 0;
 
     const keys = document.querySelectorAll('.keyboard-row button');
 
     function getNewWord(){
-        fetch('./data/wordbank_a.json')
+        // arr[Math.floor(Math.random() * arr.length)];
+        const selectionKey = 'aaabbbcccdddeeeffgghiiijkklmmnnnooppqrrrsstuuvvvwwyyzzz';
+        const selectionKeyArr = selectionKey.split('');
+        const randomSelected = selectionKeyArr[Math.floor(Math.random() * selectionKeyArr.length)];
+
+        fetch(`./data/wordbank_${randomSelected}.json`)
         .then(response => response.json())
-        .then(result => console.log(result))
+        .then(result => {
+            word = result[Math.floor(Math.random() * result.length)];
+            console.log('Solution: ', word);
+        })
         .catch(err => console.log('error', err));
+    }
+
+    function checkWord(word){
+        const firstLetter = word.charAt(0);
+        return fetch(`./data/wordbank_${firstLetter}.json`)
+               .then(response => response.json())
+               .then(result => result.includes(word));
     }
 
     function getCurrentWordArray(){
@@ -58,32 +73,45 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         const currentWord = currentWordArray.join('');
-        const firstLetterId = guessWordCount * 5 + 1;
-        const interval = 200;
 
-        currentWordArray.forEach((letter, index) => {
-            setTimeout(() => {
-                const tileColor = getTileColor(letter, index);
+        checkWord(currentWord)
+        .then(isInDatabank => {
+            if(!isInDatabank){
+                throw Error();
+            }
 
-                const letterId = firstLetterId + index;
-                const letterEL = document.getElementById(letterId);
-                letterEL.classList.add('animate__flipInX');
-                letterEL.style = `background-color: ${tileColor};border-color:${tileColor}`;
-            }, interval * index);
+            const firstLetterId = guessWordCount * 5 + 1;
+            const interval = 200;
+    
+            currentWordArray.forEach((letter, index) => {
+                setTimeout(() => {
+                    const tileColor = getTileColor(letter, index);
+    
+                    const letterId = firstLetterId + index;
+                    const letterEL = document.getElementById(letterId);
+                    letterEL.classList.add('animate__flipInX');
+                    letterEL.style = `background-color: ${tileColor};border-color:${tileColor}`;
+                }, interval * index);
+            });
+    
+            guessWordCount += 1;
+    
+            if(currentWord === word){
+                window.alert('Congratulations!');
+                return;
+            }
+    
+            if(guessedWords.length === 6){
+                window.alert(`The word you had to guess was "${word}"`);
+            }
+    
+            guessedWords.push([]);
+        })
+        .catch(err => {
+            window.alert('Word is not recognized');
         });
 
-        guessWordCount += 1;
-
-        if(currentWord === word){
-            window.alert('Congratulations!');
-            return;
-        }
-
-        if(guessedWords.length === 6){
-            window.alert(`The word you had to guess was "${word}"`);
-        }
-
-        guessedWords.push([]);
+        
     }
 
     function createTiles(){
@@ -97,6 +125,16 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function deleteLetter(){
+        const currentWordArray = getCurrentWordArray();
+        const _ = currentWordArray.pop();
+        console.log('len',guessedWords.length);
+        guessedWords[guessedWords.length - 1] = currentWordArray;
+        const lastLetterEL = document.getElementById(String(availableSpace - 1));
+        lastLetterEL.textContent = '';
+        availableSpace = availableSpace - 1;
+    }
+
     for(let i = 0; i < keys.length; i++){
         keys[i].onclick = ({target}) => {
             const letter = target.getAttribute('data-key');
@@ -104,6 +142,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 submitWord();
                 return;
             }
+
+            if(letter === 'del'){
+                console.log('delete key pressed');
+                deleteLetter();
+                return;
+            }
+
             updateGuessedWords(letter);
         }; 
     }
